99719f62e992a5b64a295fd4c0cae984e9df96f5
Nicolas <nicolasgesielfg@gmail.com>
2026-07-19 23:57:22 -0300

    feat: setup inicial do projeto BellaGestora

---
+++ src/main/java/com/nicolas/bellagestora/BellagestoraApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.bellagestora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellagestoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellagestoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolas/bellagestora/BellagestoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.bellagestora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellagestoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
